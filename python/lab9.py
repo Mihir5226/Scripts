94d19bce1682d659d5ee4f0da0aed78b4e0b6983
patelm7 <patelm7@mail.uc.edu>
2019-04-09 11:48:25 -0400

    lab 9

--- python/lab9.py
+++ python/lab9.py
@@ -12,9 +12,3 @@ print(data[0]['name'] + " " +"is" + " "+ data[0]['color'])
 print(data[1]['name'] + " " +"is" + " "+ data[1]['color'])
 print(data[2]['name'] + " " +"is" + " "+ data[2]['color'])
 print(data[3]['name'] + " " +"is" + " "+ data[3]['color'])
-
-
-
-#print(r['name'][0]['color'])
-#print(data)
-#print(items)
